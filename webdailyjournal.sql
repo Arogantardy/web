b05a2ae71a0ebfae7faf0dfdc899e4756dc7e80e
Karma <aragornsrohan@gmail.com>
2026-01-09 19:29:51 +0700

    Update webdailyjournal.sql

--- webdailyjournal.sql
+++ webdailyjournal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2025 at 04:03 PM
+-- Generation Time: Jan 09, 2026 at 01:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,7 +44,33 @@ INSERT INTO `article` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VA
 (1, 'GAG', '\"Grow a Garden\" artinya adalah \"menumbuhkan kebun\" secara harfiah, tetapi lebih dikenal sebagai nama sebuah permainan simulasi berkebun viral di platform Roblox. Dalam permainan ini, pemain bisa menanam, merawat, dan memanen tanaman virtual untuk mendapatkan uang (disebut Shackles) yang kemudian digunakan untuk mengembangkan kebun mereka dengan bibit dan hewan langka.', 'tirek.png', '2025-12-07 14:12:46', 'admin'),
 (2, 'ZEN', 'Zenin Event\r\nZenin Event Grow a Garden adalah acara terbatas waktu dalam gim video Grow a Garden di platform Roblox, yang berlangsung dari 19 Juli 2025 hingga 2 Agustus 2025. Acara ini berfokus pada pengumpulan mata uang baru bernama Zen dengan cara mengumpulkan Tanaman Tenang (Tranquil Plants) yang muncul selama Acara Zen Aura, lalu menukarkannya di NPC biksu untuk membuka hadiah bertema Jepang seperti kosmetik dan hewan peliharaan baru.', 'koi.png', '2025-12-07 14:13:54', 'admin'),
 (4, 'Anis turu', 'Alomani jam 12 keturon dc', '20251211092921.png', '2025-12-11 09:29:21', 'admin'),
-(5, 'saya banh', 'Si nyonyok ngide', '20251211093337.png', '2025-12-11 09:33:37', 'admin');
+(7, 'saya banh', 'erin nyonyok ngide', '20260103021809.png', '2026-01-03 02:18:09', 'manajer1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL,
+  `judul` text CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `gambar` text CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `judul`, `gambar`, `tanggal`, `username`) VALUES
+(3, 'saya banh', '20260109072940.png', '2026-01-09', 'admin'),
+(4, 'animek', '20260108213254.jpg', '2026-01-08', 'admin'),
+(5, 'animek 2', '20260108213306.jpg', '2026-01-08', 'admin'),
+(6, 'animek3', '20260108213319.jpg', '2026-01-08', 'admin'),
+(7, 'narugacuga', '20260108213341.jpg', '2026-01-08', 'admin'),
+(8, 'saya banh', '20260109183400.png', '2026-01-09', 'danny');
 
 -- --------------------------------------------------------
 
@@ -64,7 +90,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
-(1, 'admin', '827ccb0eea8a706c4c34a16891f84e7b', '');
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '20260109030826.jpg'),
+(5, 'fajri', '202cb962ac59075b964b07152d234b70', ''),
+(6, 'dika', '202cb962ac59075b964b07152d234b70', ''),
+(7, 'bowok', '202cb962ac59075b964b07152d234b70', ''),
+(8, 'danny', '21232f297a57a5a743894a0e4a801fc3', ''),
+(9, 'ardi', '202cb962ac59075b964b07152d234b70', '');
 
 --
 -- Indexes for dumped tables
@@ -74,6 +105,12 @@ INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
 -- Indexes for table `article`
 --
 ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -90,13 +127,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
